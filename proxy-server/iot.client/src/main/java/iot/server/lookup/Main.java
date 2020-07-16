b328e030ab044ed7689f79af6233c4f57a129f69
guidogagl <gagliardi.guido.96@gmail.com>
2020-07-16 14:23:51 -0400

    blabla

--- proxy-server/iot.client/src/main/java/iot/server/lookup/Main.java
+++ proxy-server/iot.client/src/main/java/iot/server/lookup/Main.java
@@ -7,11 +7,10 @@ public class Main{
 	static final int port = 5673;
 	static final CoapServer server = new CoapServer( port );
 
-	static final CaliforniumLogger cl = new CaliforniumLogger();
+	static CaliforniumLogger cl = new CaliforniumLogger();
 
 	public static void main(String args[]) {
 		System.out.println("Starting CoapServer....");
-		
 		cl.disableLogging();
 
 		server.add( new MyLookUpResource("lookup"));
